40ef88019eed39ef85a59bcd44848715e9318b96
AaronTemi112 <certifiedtemzzz@gmail.com>
2023-03-03 14:47:28 +0000

    App commit

---
+++ ConsoleApp.Test/UnitTest1.cs
@@ -0,0 +1,22 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleAppProject.App01;
+
+namespace ConsoleApp.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+    
+             public void TestConvertFeetToMiles()
+            {
+             DistanceConverter converter = new DistanceConverter();
+
+            converter.inputValue = DistanceConverter.FEET_TO_MILES;
+
+
+
+
+        }
+        }
+    }
